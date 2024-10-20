9eebb882f2c015bc3296f6f1e42019e2f620b617
Ayush <aayushchauhan1911@gmail.com>
2024-10-20 19:59:18 +0530

    testing1

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-console.log('Hello world');
+console.log('Hello world how are you');
 let name="Ayush Chauhan....";
 let age=21;
 let catColor="black";
-let catColorr="Mix";
+let catColorr="partial";
 console.log(name,age,catColor,catColorr);
